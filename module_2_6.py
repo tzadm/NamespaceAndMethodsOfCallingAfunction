23035ef3c95680ffffb6a0db625a6469471caa51
tzadm <94993624+tzadm@users.noreply.github.com>
2024-05-26 18:37:52 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -16,9 +16,8 @@ def single_root_words(root_world, *other_words):
         if i in root_world:
             same_words.append(i)
         elif i not in root_world:
-            d = str(i).lower()
-            i = d
-            if d in root_world:
+            i = str(i).lower()
+            if i in root_world:
                 same_words.append(i.title())
             elif i not in root_world:
                 root_world = str(root_world).lower()
